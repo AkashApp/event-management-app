2176b4fcdcfb8c6555022e8d8be2afe0f2927839
akash-borate <akash.borate@parkmate.in>
2025-04-08 19:29:37 +0530

    register and login is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  // {
+  //   path: '',
+  //   component: MainLayoutComponent, // 👈 protected area
+  //   canActivate: [AuthGuard],
+  //   children: [
+  //     {
+  //       path: 'events',
+  //       loadChildren: () =>
+  //         import('./events/events.module').then((m) => m.EventsModule),
+  //     },
+  //     { path: '', redirectTo: 'events', pathMatch: 'full' },
+  //   ],
+  // },
+  { path: '**', redirectTo: 'auth/login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
